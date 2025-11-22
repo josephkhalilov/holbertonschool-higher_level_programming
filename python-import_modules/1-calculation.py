443abf093293f15b019bcb1bccced3ccc40d0386
babapro031 <stormgainucun1@gmail.com>
2025-11-22 17:57:19 +0400

    Update 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-import calculator_1 as cal1
-a=10
-b=5
-result1=cal1.add(a, b)
-result2=cal1.sub(a, b)
-result3=cal1.mul(a, b)
-result4=cal1.div(a, b)
-print(f"{a} + {b} = {result1}")
-print(f"{a} - {b} = {result2}")
-print(f"{a} * {b} = {result3}")
-print(f"{a} / {b} = {result4}")
+
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+
+    print("{} + {} = {}".format(a, b, result_add))
+    print("{} - {} = {}".format(a, b, result_sub))
+    print("{} * {} = {}".format(a, b, result_mul))
+    print("{} / {} = {}".format(a, b, result_div))
