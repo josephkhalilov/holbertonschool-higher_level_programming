b19e6478f2e9ac6efe9b8f64bc71a50e2e76b3cb
babapro031 <stormgainucun1@gmail.com>
2025-11-29 08:53:30 +0000

    fewfewf

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+class BaseGeometry:
+    """Base geometry class."""
+
+    def area(self):
+        """Raise an exception for unimplemented area."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer."""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
